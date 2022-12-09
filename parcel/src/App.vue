228fc4c3a4eed800452314260439900dd12598ff
suhwa <hansuhwa2005@gmail.com>
2022-12-09 11:48:10 +0900

    parcel setting

---
+++ parcel/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello Vue!",
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+$color: orange;
+
+h1 {
+  color: $color;
+}
+</style>
